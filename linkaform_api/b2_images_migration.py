83f52480467d5d5a847b727ddc65df1cc4d5e33b
Infosync Develop Team <develop@info-sync.com>
2017-02-22 23:49:09 -0600

    se agrego busquedas mensuales

--- linkaform_api/b2_images_migration.py
+++ linkaform_api/b2_images_migration.py
@@ -149,113 +149,117 @@ postgres_port = '5432'
 
 records_with_errors = {}
 databases_with_errors = []
-date = datetime.strptime('2015-09-13 00:00:00', "%Y-%m-%d %H:%M:%S")
-date2 = datetime.strptime('2015-09-15 00:00:00', "%Y-%m-%d %H:%M:%S")
 
 dbpath='/backup/infosync/2017/backup/infosync/2017/Mongoinfosync-ALLClientDB-2017-20'
 
-for dbname in databases:
-    #dbname.strip('\n')
-    if dbname in ['infosync', 'local']:
-        continue
-    cur_db = mongo_util.connect_mongodb(dbname, host, port)
-    # cur_db = mongo_util.connect_mongodb(dbname, '127.0.0.1', port)
-    cur_col = mongo_util.get_mongo_collection(cur_db, collection_name)
-    # cur_col_orig = mongo_util.get_mongo_collection(cur_db_orig, collection_name)
 
-    query = {'deleted_at':{'$exists':0}, 'created_at':{'$gte':date,'$lt':date2}}
-    # query = {'deleted_at':{'$exists':0}, 'created_at':{'$gte':date}}
-    # query = {'form_id':561,'deleted_at':{'$exists':0}}
-    # result = mongo_util.get_collection_objects(cur_col_orig, query)
-    print 'query=',query
-    result = mongo_util.get_collection_objects(cur_col, query)
-    print result.count()
-    records = [record for record in result]
-    i = 0
-    print 'dbname', dbname
-    for record in records:
-        i += 1
-        if i % 300 == 0:
-            print i
-        if not record.get('answers', None):
-            continue
-        ##### en los registos mas viejos del 2016 no hay porque copiar la info
-        # cur_col.update(
-        #      {'_id':ObjectId(record['_id'])},
-        #         {
-        #           "$set": {
-        #                    'answers': record['answers']
-        #                 }
-        #           } )
-        for _key in record['answers']:
-            if isinstance(record['answers'][_key], dict):
-                if 'file_url' in record['answers'][_key].keys():
-                    new_url = None
-                    already_in_b2 = False
-                    file_url = record['answers'][_key]['file_url']
-                    if file_url == False:
-                        #print 'record_id', record['_id']
-                        continue
-                    if file_url and 'backblazeb2' in file_url:
-                        if absolute_path in file_url:
-                            already_in_b2 = True
-                            file_url = file_url.replace(absolute_path, '')
-                        else:
-                            continue
-                    elif file_url and absolute_path in file_url:
-                        last = file_url.rfind(absolute_path)
-                        if last == 0:
-                            continue
-                        else:
-                            file_url = file_url.replace(absolute_path, '')
+for year in [2016,2015,2014]:
+    for month in range(11):
+        month += +1
+        date_from = datetime.strptime('2015-%s-01 00:00:00'%month, "%Y-%m-%d %H:%M:%S")
+        date_to = datetime.strptime('2015-%s-01 00:00:00'%(month+1), "%Y-%m-%d %H:%M:%S")
+        for dbname in databases:
+            #dbname.strip('\n')
+            if dbname in ['infosync', 'local']:
+                continue
+            cur_db = mongo_util.connect_mongodb(dbname, host, port)
+            # cur_db = mongo_util.connect_mongodb(dbname, '127.0.0.1', port)
+            cur_col = mongo_util.get_mongo_collection(cur_db, collection_name)
+            # cur_col_orig = mongo_util.get_mongo_collection(cur_db_orig, collection_name)
 
-                    if not already_in_b2:
-                        record['answers'][_key]['file_url'] = absolute_path + file_url
-                    else:
-                        record['answers'][_key]['file_url'] = file_url
-                    #print record['answers'][_key]['file_url']
-                    cur_col.update(
-                        {'_id':ObjectId(record['_id'])},
-                        {
-                            "$set": {
-                                'answers.' + _key: record['answers'][_key]
-                                     } })
-
-            elif isinstance(record['answers'][_key], list):
-                new_group = []
-                for group in record['answers'][_key]:
-                    if isinstance(group, dict):
-                        for group_key in group.keys():
-                            if isinstance(group[group_key], dict) and 'file_url' in group[group_key]:
-                                new_url = None
-                                file_url = group[group_key]['file_url']
-                                # print file_url
-                                # Change to absolute path
-                                if file_url and 'backblazeb2' in file_url:
-                                    if absolute_path in file_url:
-                                        already_in_b2 = True
-                                        file_url = file_url.replace(absolute_path, '')
-                                    else:
-                                        continue
-                                elif file_url and absolute_path in file_url:
-                                    last = file_url.rfind(absolute_path)
-                                    if last == 0:
-                                        continue
-                                    else:
-                                        file_url = file_url.replace(absolute_path, '')
-                                if not already_in_b2:
-                                    group[group_key]['file_url'] = absolute_path + file_url
+            query = {'deleted_at':{'$exists':0}, 'created_at':{'$gte':date_from,'$lt':date_to}}
+            # query = {'deleted_at':{'$exists':0}, 'created_at':{'$gte':date}}
+            # query = {'form_id':561,'deleted_at':{'$exists':0}}
+            # result = mongo_util.get_collection_objects(cur_col_orig, query)
+            print 'query=',query
+            result = mongo_util.get_collection_objects(cur_col, query)
+            print result.count()
+            records = [record for record in result]
+            i = 0
+            print 'dbname', dbname
+            for record in records:
+                i += 1
+                if i % 300 == 0:
+                    print i
+                if not record.get('answers', None):
+                    continue
+                ##### en los registos mas viejos del 2016 no hay porque copiar la info
+                # cur_col.update(
+                #      {'_id':ObjectId(record['_id'])},
+                #         {
+                #           "$set": {
+                #                    'answers': record['answers']
+                #                 }
+                #           } )
+                for _key in record['answers']:
+                    if isinstance(record['answers'][_key], dict):
+                        if 'file_url' in record['answers'][_key].keys():
+                            new_url = None
+                            already_in_b2 = False
+                            file_url = record['answers'][_key]['file_url']
+                            if file_url == False:
+                                #print 'record_id', record['_id']
+                                continue
+                            if file_url and 'backblazeb2' in file_url:
+                                if absolute_path in file_url:
+                                    already_in_b2 = True
+                                    file_url = file_url.replace(absolute_path, '')
                                 else:
-                                    group[group_key]['file_url'] = file_url
-                                print group[group_key]['file_url']
-                        new_group.append(group)
-                cur_col.update(
-                            {'_id':ObjectId(record['_id'])},
-                            {
-                                "$set": {
-                                    "answers." + _key: new_group
-                                }
-                            } )
-    # cr.drop_database(dbname)
+                                    continue
+                            elif file_url and absolute_path in file_url:
+                                last = file_url.rfind(absolute_path)
+                                if last == 0:
+                                    continue
+                                else:
+                                    file_url = file_url.replace(absolute_path, '')
+
+                            if not already_in_b2:
+                                record['answers'][_key]['file_url'] = absolute_path + file_url
+                            else:
+                                record['answers'][_key]['file_url'] = file_url
+                            #print record['answers'][_key]['file_url']
+                            cur_col.update(
+                                {'_id':ObjectId(record['_id'])},
+                                {
+                                    "$set": {
+                                        'answers.' + _key: record['answers'][_key]
+                                             } })
+
+                    elif isinstance(record['answers'][_key], list):
+                        new_group = []
+                        for group in record['answers'][_key]:
+                            if isinstance(group, dict):
+                                for group_key in group.keys():
+                                    if isinstance(group[group_key], dict) and 'file_url' in group[group_key]:
+                                        new_url = None
+                                        file_url = group[group_key]['file_url']
+                                        # print file_url
+                                        # Change to absolute path
+                                        if file_url and 'backblazeb2' in file_url:
+                                            if absolute_path in file_url:
+                                                already_in_b2 = True
+                                                file_url = file_url.replace(absolute_path, '')
+                                            else:
+                                                continue
+                                        elif file_url and absolute_path in file_url:
+                                            last = file_url.rfind(absolute_path)
+                                            if last == 0:
+                                                continue
+                                            else:
+                                                file_url = file_url.replace(absolute_path, '')
+                                        if not already_in_b2:
+                                            group[group_key]['file_url'] = absolute_path + file_url
+                                        else:
+                                            group[group_key]['file_url'] = file_url
+                                        print group[group_key]['file_url']
+                                new_group.append(group)
+                        cur_col.update(
+                                    {'_id':ObjectId(record['_id'])},
+                                    {
+                                        "$set": {
+                                            "answers." + _key: new_group
+                                        }
+                                    } )
+            # cr.drop_database(dbname)
 print 'records_with_errors=', records_with_errors
 print 'dbs_with_errors=', databases_with_errors
