05f6dac19700d405a3cc6d4641c7b4e2ef428619
Linkaform Develop Team <backend@linkaform.com>
2023-11-06 21:39:53 -0600

    se agrego la parte de reportes

--- linkaform_api/lkf_base/base.py
+++ linkaform_api/lkf_base/base.py
@@ -15,6 +15,10 @@ class LKF_Base(LKFBaseObject):
         self.lkf_base = {}
         self._set_connections(settings)
         self.current_record = {}
+        self.close_status = 'close'
+        self.open_status = 'open'
+        self.open_status = 'new'
+        self.status_id = '0000000000000000000aaaaa'
         settings = self.update_settings(settings)
         if sys_argv:
             self.argv = sys_argv
@@ -23,7 +27,7 @@ class LKF_Base(LKFBaseObject):
                 config['JWT_KEY'] = self.data["jwt"].split(' ')[1]
                 config['USER_JWT_KEY'] = self.data["jwt"].split(' ')[1]
                 settings.config.update(config)
-            self.current_record = self.get_record(sys_argv)
+            self.current_record = self.get_current_record(sys_argv)
             self._set_connections(settings)
 
     def _set_connections(self, settings):
@@ -33,15 +37,34 @@ class LKF_Base(LKFBaseObject):
         self.lkm = lkf_models.LKFModules(settings)
         return True
 
+    def do_records_close(self, form, folio, status_id=None):
+        res = self.is_record_close(form, folio)
+        if not res:
+            res = self.record_close(form, folio, 
+                        status_id= self.get_key_id(status_id),
+                        value=self.get_value() 
+                        )
+        return res
+
+    def do_records_open(self, form, folio,status_id=None):
+        return self.set_record(form, folio, 
+                    status_id=self.get_key_id(), 
+                    value=self.get_value() 
+                )
+
     def cache_drop(self, query):
         return self.delete(query=query)
 
     def cache_get(self, values, **kwargs):
-        res = self.search(values)
+        res = self.cache_read(values, **kwargs)
         if res and res.get('_id'):
             if kwargs.get('keep_cache'):
                 return res
             self.cache_drop({'_id':res['_id']})
+        return res
+
+    def cache_read(self, values, **kwargs):
+        res = self.search(values)
         return res
 
     def cache_set(self, values):
@@ -55,7 +78,6 @@ class LKF_Base(LKFBaseObject):
         return self.create({'_id':t_id, 'cache':values})
 
     def cache_update(self, values):
-        print('drop cache_update....')
         self.create({'test':1,'status':'drop'})
 
     def console_run(self):
@@ -70,7 +92,7 @@ class LKF_Base(LKFBaseObject):
         if len(value) == 10:
             #Date
             value = get_value(value, '%Y-%m-%d')
-        elif len(value) == 19:
+        elif len(value) == 19 or len(value) == 18:
             value = get_value(value, '%Y-%m-%d %H:%M:%S')
         elif len(value) == 16:
             value = get_value(value, '%Y-%m-%d %H:%M')
@@ -129,6 +151,16 @@ class LKF_Base(LKFBaseObject):
         wget.download(file_url, '/tmp/{}'.format(file_name))
         return file_name
 
+    def get_answer_value(self, key_id=None, value=None):
+        if not key_id:
+            key_id = self.get_key_id()
+        if not value:
+            value = self.get_value()
+        if key_id.find('.'):
+            #TODO hacer busqeuda dentro de grupos
+            key=key_id
+        return {f'answers.{self.get_key_id(key)}':self.get_value(value)}
+
     def get_date_str(self, value):
         if value:
             if isinstance(value, datetime):
@@ -143,7 +175,7 @@ class LKF_Base(LKFBaseObject):
         records = self.cr.aggregate(query)
         return [r for r in records ]
 
-    def get_record(self, sys_argv):
+    def get_current_record(self, sys_argv):
         try:
             record_to_long = eval(sys_argv[3])
         except:
@@ -155,14 +187,41 @@ class LKF_Base(LKFBaseObject):
             current_record = self.get_record_from_db( current_record.get('folio'), current_record.get('folio') )
         return current_record
 
-    def get_record_from_db(self, form_id, folio):
-        query = {
-            'form_id': form_id,
-            'folio': folio,
-            'deleted_at': {'$exists': False}
-        }
-        select_columns = {'folio':1,'user_id':1,'form_id':1,'answers':1,'_id':1,'connection_id':1,'created_at':1,'other_versions':1,'timezone':1}
-        record_found = self.cr.find(query, select_columns)
+    def get_key_id(self, key_id=None):
+        if not key_id:
+            key_id = self.status_id
+        return key_id
+
+    def get_records(self, form_id=None, folio=None, query_answers={}, select_columns=[]):
+        if select_columns:
+            select_columns = {key:1 for key in select_columns}
+        else:
+            select_columns = {'folio':1,'user_id':1,'form_id':1,'answers':1,'_id':1,'connection_id':1,'created_at':1,'other_versions':1,'timezone':1}
+        match_query = {'deleted_at': {'$exists': False}}
+    
+        if form_id:
+            match_query.update(self.get_query_by('form_id', form_id))
+        if folio:
+            match_query.update(self.get_query_by('folio', folio))
+        if query_answers:
+            match_query.update(query_answers)
+        return self.cr.find(match_query, select_columns)
+
+    def get_record_from_db(self, form_id=None, folio=None, query_answers={}, select_columns=[]):
+        if select_columns:
+            select_columns = {key:1 for key in select_columns}
+        else:
+            select_columns = {'folio':1,'user_id':1,'form_id':1,'answers':1,'_id':1,'connection_id':1,'created_at':1,'other_versions':1,'timezone':1}
+        match_query = {'deleted_at': {'$exists': False}}
+    
+        if form_id:
+            match_query.update(self.get_query_by('form_id', form_id))
+        if folio:
+            match_query.update(self.get_query_by('folio', folio))
+        if query_answers:
+            match_query.update(query_answers)
+
+        record_found = self.cr.find(match_query, select_columns)
         return record_found.next()
 
     def get_record_by_id(self, _id):
@@ -199,6 +258,17 @@ class LKF_Base(LKFBaseObject):
         else:
             return {}
 
+    def get_query_by(self, key, value):
+        update = {key: value}
+        if type(value) == list:
+            update = {key: {'$in': value}}
+        return update
+
+    def get_value(self, get_value=None):
+        if not get_value:
+            get_value = self.close_status
+        return get_value
+
     def get_date_query(self, date_from=None, date_to=None, date_field=None, date_field_id=None, field_type=None):
         res = {}
         if not date_field:
@@ -234,10 +304,37 @@ class LKF_Base(LKFBaseObject):
             })
         return res
 
+    def is_record_close(self, form, folio, status_id=None ):
+        match_query = {'deleted_at': {'$exists': False}}
+        match_query.update(self.get_query_by('form_id', form))
+        match_query.update(self.get_query_by('folio', folio))
+        match_query.update(self.get_query_by(f'answers.{self.get_key_id()}',self.get_value()))
+        res = self.cr.find_one(match_query)
+        if res:
+            return res
+        else:
+            return False
+
     def read_current_record_from_txt(self, file_url):
         name_downloded = self.download_pdf( file_url, is_txt=True )
         f = open( "/tmp/{}".format( name_downloded ) )
         return simplejson.loads( f.read() )
+
+    def record_close(self, form, folio, status_id=None, value=None ):
+        if not status_id:
+            status_id = self.get_key_id()
+        if value: 
+            value = {'$set':self.get_answer_value(value)}
+        else:
+            value = {'$set':self.get_answer_value(status_id)}
+        return self.set_record(form, folio, status_id=status_id, value=value )
+
+    def set_record(self, form, folio, status_id=None, value=None ):
+        match_query = {'deleted_at': {'$exists': False}}
+        match_query.update(self.get_query_by('form_id', form))
+        match_query.update(self.get_query_by('folio', folio))
+        update_db = self.cr.update_many(match_query, value)
+        return update_db.raw_result
 
     def update_settings(self, settings):
         lkf_api = utils.Cache(settings)
@@ -280,3 +377,34 @@ class LKF_Base(LKFBaseObject):
         else:
             raise('Not a valid length of a date')
         return value
+
+
+#####
+
+
+
+
+
+class LKF_Report(LKF_Base):
+
+
+    def __init__(self, settings, sys_argv=None, use_api=False):
+        print('INIT LKF_Report....')
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
+        self.json = {
+            # "firstElement":{
+            #     "data": [],
+            # },
+            # "secondElement":{
+            #     "data": [],
+            # },
+            # "firstFilter":[],
+            # "secondFilter":[]
+        } 
+
+    def report_print(self):
+        res = {'json':{}}
+        for x in self.json:
+            res['json'][x] = self.json[x]
+        return res
+
