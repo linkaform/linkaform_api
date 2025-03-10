ab433c75af57f95ebda1973f48d4bab80e0c99c1
Roman Lezama <roman.lezama@linkaform.com>
2025-03-10 09:23:13 -0600

    Se envia from_api = True en la funcion de asignacion de registros

--- linkaform_api/utils.py
+++ linkaform_api/utils.py
@@ -66,9 +66,10 @@ class Cache(object):
     def assigne_connection_records(self, connection_id, record_id_list, user_of_connection=False,
         send_email=False, send_push_notification=False, jwt_settings_key=False):
         url_method = self.api_url.record['assigne_connection']
+        print('enviando from_api True')
         data = {'connection_id': connection_id, 'records': record_id_list,
                   'send_push_notification': send_push_notification,
-                  'send_mail': send_email}
+                  'send_mail': send_email, 'from_api': True}
         if user_of_connection:
             data['userOfConnection'] = user_of_connection
 
