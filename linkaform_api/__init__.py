653aa879b9114ce83323f56de273f2e41dad9d65
Linkaform Develop Team <backend@linkaform.com>
2025-12-05 11:43:10 -0600

    refactor de version 2

--- linkaform_api/__init__.py
+++ linkaform_api/__init__.py
@@ -1,13 +1,9 @@
-from .models import base_models
 from .utils import Cache
-from .odoo import Odoo
 from .network import Network
 from .upload_file import LoadFile
 from .couch_util import Couch_utils
-from .lkf_object import LKFBaseObject
+# from .lkf_object import LKFBaseObject
 from .settings import *
-from .models import lkf_models
-from .lkf_base import base
 from .config import *
 from .generar_qr import *
 import sys
